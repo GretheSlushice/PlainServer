968856c8189d4138a2a1fb529c1e85d20f611252
Grethe Slushice <simonshn.gmh@gmail.com>
2019-09-10 13:08:04 +0200

    Initial commit

---
+++ ModelLib/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ModelLib
+{
+    public class Car
+    {
+        public string Model;
+        public string Color;
+        public string RegNumber;
+
+        public Car(string model, string color, string regNumber)
+        {
+            Model = model;
+            Color = color;
+            RegNumber = regNumber;
+        }
+
+        public override string ToString()
+        {
+            return Model +", " + Color+ ", " + RegNumber;
+        }
+    }
+}
---
+++ PlainClient/Client.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using ModelLib;
+
+namespace PlainClient
+{
+    public class Client
+    {
+        public void Start()
+        {
+            Car myCar = new Car("Tesla", "Red", "EL3400");
+            
+
+            using (TcpClient socket = new TcpClient("localhost", 10001))
+            using (StreamWriter sw = new StreamWriter(socket.GetStream()))
+            using (StreamReader sr = new StreamReader(socket.GetStream()))
+            {
+                sw.WriteLine(myCar);
+                sw.Flush();
+            }
+        }
+    }
+}
